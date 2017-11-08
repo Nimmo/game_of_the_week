653683236b8d169beb319ef90072d23ba048422b
Graeme Nimmo <graeme@nimmog.co.uk>
2017-11-08 22:38:31 +0000

    New: Can now play videos as screenshot of the week.

--- main.py
+++ main.py
@@ -1,10 +1,37 @@
 from PIL import Image, ImageTk
-import tkinter as tk
+import imageio
+import tkinter as tk, threading
 import os
 import json
+import sys
 
 
+def stream(label):
 
+    for image in video.iter_data():
+        frame_image = ImageTk.PhotoImage(Image.fromarray(image))
+        label.config(image=frame_image)
+        label.image = frame_image
+
+        
+def prepare_file(infile, size):
+    image_types = ["png", "jpg"]
+    filetype = infile.split(".")[-1]
+    if filetype in image_types:
+        filetype = "image"
+        outfile = os.path.splitext(infile)[0] + ".thumbnail"
+        if infile != outfile:
+            try:
+                img = Image.open(infile)
+                img.thumbnail(size, Image.ANTIALIAS)
+            except IOError:
+                print("cannot create thumbnail for '%s'" % infile)
+                sys.exit()
+    else:
+        filetype = "video"
+        img = infile            
+
+    return img, filetype
 
 if __name__ == "__main__":
     try:
@@ -22,6 +49,9 @@ if __name__ == "__main__":
         ssotw = input("Please enter the filename for the game of the week's screenshot: ")
         settings = {"gotw": gotw, "ssotw": ssotw}
         json.dump(settings, open("settings.json","w"))
+    except:
+        print("An unexpected error occurred, exiting gracefully.")
+        sys.exit()
 
     root = tk.Tk()
     root.attributes("-fullscreen", True)
@@ -32,18 +62,24 @@ if __name__ == "__main__":
     height = 400
 
     size = width, height-60
-    outfile = os.path.splitext(infile)[0] + ".thumbnail"
-    if infile != outfile:
-        try:
-            img = Image.open(infile)
-            img.thumbnail(size, Image.ANTIALIAS)
-        except IOError:
-            print("cannot create thumbnail for '%s'" % infile)
-
-    tkimage = ImageTk.PhotoImage(img)
+    
+    screenshot, filetype = prepare_file(infile, size)    
 
     root.geometry('{}x{}'.format(width, height+20))
     game_of_the_week_label = tk.Label(root, text="Game of the week:", font=("Courier", 44), bg="black", fg="white").pack()
-    tk.Label(root, image=tkimage).pack()
+
+    if filetype == "image":
+        tkscreenshot = ImageTk.PhotoImage(screenshot)
+        tk.Label(root, image=tkscreenshot).pack()
+    else:
+        video = imageio.get_reader(screenshot)
+        my_label = tk.Label(root)
+        my_label.pack()
+        thread = threading.Thread(target=stream, args=(my_label,))
+        thread.daemon = 1
+        thread.start()
+
+    
+    
     gamelabel = tk.Label(root, text=gotw, font=("Courier", 44), bg="black", fg="white").pack()
     root.mainloop()
